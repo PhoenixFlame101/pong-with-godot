fe5b0f3aafee919a21594c0a6525cbb5ef2edfed
tnayagarwal <tanaynilesh@gmail.com>
2022-10-21 23:29:38 +0530

    making game faster

--- logic/ball.gd
+++ logic/ball.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-export var DEFAULT_SPEED = 200
+export var DEFAULT_SPEED = 235
 
 var _speed = DEFAULT_SPEED
 var direction = Vector2.LEFT
@@ -9,7 +9,7 @@ var death_count = 0
 onready var _initial_pos = position
 
 func _process(delta):
-	_speed += delta * 2
+	_speed += delta * 2.4
 	position += _speed * delta * direction
 
 
