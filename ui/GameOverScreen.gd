290c621fcaee9b84a2d48032e579421b030524a2
Varun Kamath <varunkamath05@gmail.com>
2022-10-22 03:06:52 +0530

    #3- Quit Screen (Better Looking)
    Made it look better pls ignore the main menu I will fix that also and push in other branch since its another issue also

--- ui/GameOverScreen.gd
+++ ui/GameOverScreen.gd
@@ -1,10 +1,9 @@
 extends CanvasLayer
 
 
+func _on_Quit_pressed():
+	get_tree().quit();
+
 
 func _on_Restart_pressed():
 	get_tree().change_scene("res://pong.tscn");
-
-
-func _on_Quit_pressed():
-	get_tree().quit();
